869e83e3ccbe83171a03c2d9a9bd8c028aa30887
santa51107HD <santa51107@gmail.com>
2024-09-09 17:39:10 -0500

    API para actualizar la informacion de la base de datos por medio de un archivo excel

--- edufeedbackai/urls.py
+++ edufeedbackai/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import EvaluacionesDocenteView,  CreateTokenView, AnalyzeCommentsView
+from core.views import EvaluacionesDocenteView,  CreateTokenView, AnalyzeCommentsView, ExcelUploadView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', CreateTokenView.as_view(), name='login'), #POST
     path('evaluaciones/docente/', EvaluacionesDocenteView.as_view(), name='evaluaciones_docente'), #GET
     path('analizar/comentarios/', AnalyzeCommentsView.as_view(), name='analizar_comentarios'),  # POST
+    path('upload-excel/', ExcelUploadView.as_view(), name='upload-excel'), # POST
 ]
